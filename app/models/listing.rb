f6c0c753d7f2614cb97e58e9b254a2da161b2a65
Eliza Khachatryan <elijbet@gmail.com>
2017-04-02 12:42:07 -0700

    add validations for users and listings, paperclip validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,5 +9,9 @@ class Listing < ApplicationRecord
     									:path => ":style/:id_:filename"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	end
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
 
