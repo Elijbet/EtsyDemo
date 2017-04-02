75d647515b76905f5d52e955c814874c419813ec
Eliza Khachatryan <elijbet@gmail.com>
2017-04-01 17:32:58 -0700

    Fixing error

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
-	has_attached_file :image, styles: { medium: "200x", big: "400x", thumb: "100x100>" }, default_url: "default.jpg"
+	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     has_attached_file :image,
