d959fff68678ecec6119672d473dd62afc7c58aa
Eliza Khachatryan <elijbet@gmail.com>
2017-04-01 17:55:11 -0700

    configure dropbox to be used only in production

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,13 @@
 class Listing < ApplicationRecord
 	if Rails.env.development?
 		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
-	else
-		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
-											:storage => :dropbox,
-    									:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  else
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg",
+    									:storage => :dropbox,
+    									:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     									:path => ":style/:id_:filename"
-  end
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	end
 end
+
